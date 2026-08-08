5c77336c9d3706dd40686b95e4b87637b628cb36
Rudran Chakraborty <rudranchakraborty@mac.code>
2026-08-08 13:54:30 +0200

    feat: cofigure tabspace

--- init.lua
+++ init.lua
@@ -139,6 +139,12 @@ vim.o.timeoutlen = 300
 vim.o.splitright = true
 vim.o.splitbelow = true
 
+-- Configure Tab space
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
